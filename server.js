363c098cf78ef387c0b6dd86819ce33decbd423e
Ishaan Gupta <igupta097@gmail.com>
2021-01-10 16:48:58 +0530

    initial commit - stripe payment

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const cors = require('cors');
+const path = require('path');
+
+if(process.env.NODE_ENV!== 'production') require('dotenv').config();
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use(cors());
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.get('*', function(req, res){
+        res.send(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
+
+
+app.post("/payment", (req, res)=>{
+    console.log(req.body)
+    const body = {
+        source: req.body.token.id,
+        amount : req.body.amount,
+        currency : 'inr',
+        description : 'iSkull Mart Services Testing'
+    };
+
+    stripe.charges.create(body).then(stripeRes => {
+        res.status(200).send({success : stripeRes})
+    }).catch(stripeError => {
+        console.log(stripeError)
+        res.status(500).send({error : stripeError})
+    })
+})
+
+app.listen(PORT, error =>{
+    if (error) throw error;
+    console.log("Server running on PORT : "+PORT);
+});
